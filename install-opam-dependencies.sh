f21532755f7cefb56c8a7b95d675f07215c99ad9
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2018-10-15 18:28:37 +0100

    version all opam dependencies and add merlin

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 opam install -y \
-    cmdliner \
-    ctypes \
-    ctypes-foreign \
-    dune \
-    num \
-    ocamlbuild \
-    ocp-indent \
-    octavius \
-    result \
-    seq
+    cmdliner.1.0.2 \
+    ctypes.0.14.0 \
+    ctypes-foreign.0.4.0 \
+    dune.1.2.1 \
+    merlin.3.1.0 \
+    num.1.1 \
+    ocamlbuild.0.12.0 \
+    ocp-indent.1.6.1 \
+    octavius.1.2.0 \
+    result.1.3 \
+    seq.base
