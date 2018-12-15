be09f53dc5dee8b802a2b3b2cd0c74f4a83c46af
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2018-12-15 15:51:30 +0000

    jane-120.11+73

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 opam install -y \
-    cmdliner.1.0.2 \
+    cmdliner.1.0.3 \
     ctypes.0.14.0 \
     ctypes-foreign.0.4.0 \
-    dune.1.4.0 \
+    dune.1.5.1 \
     merlin.3.2.2 \
     num.1.1 \
     ocamlbuild.0.12.0 \
-    ocp-indent.1.6.1 \
+    ocp-indent.1.7.0 \
     octavius.1.2.0 \
     result.1.3 \
     seq.base
