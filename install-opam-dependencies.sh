4b47dcfb05c268bbc716ee43b02c99c105d7c6af
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2019-01-20 15:11:02 +0000

    jane-120.19+170

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -4,7 +4,7 @@ opam install -y \
     cmdliner.1.0.3 \
     ctypes.0.14.0 \
     ctypes-foreign.0.4.0 \
-    dune.1.5.1 \
+    dune.1.6.3 \
     merlin.3.2.2 \
     num.1.1 \
     ocamlbuild.0.12.0 \
