d1af1451ae6ee292507b6d16d65965ca792f973d
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2019-03-03 08:59:38 +0000

    update opam versions

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -4,10 +4,10 @@ opam install -y \
     cmdliner.1.0.3 \
     ctypes.0.14.0 \
     ctypes-foreign.0.4.0 \
-    dune.1.6.3 \
+    dune.1.7.2 \
     merlin.3.2.2 \
     num.1.1 \
-    ocamlbuild.0.12.0 \
+    ocamlbuild.0.14.0 \
     ocp-indent.1.7.0 \
     octavius.1.2.0 \
     result.1.3 \
