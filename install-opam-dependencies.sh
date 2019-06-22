181768ab8b6dced3090ea5918f887a501c83db20
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2019-06-22 17:40:01 +0100

    jane-120.41+154

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 opam install -y \
-    cmdliner.1.0.3 \
+    cmdliner.1.0.4 \
     ctypes.0.14.0 \
     ctypes-foreign.0.4.0 \
-    dune.1.7.2 \
-    merlin.3.2.2 \
-    num.1.1 \
+    dune.1.10.0 \
+    merlin.3.3.1 \
+    num.1.2 \
     ocamlbuild.0.14.0 \
     ocp-indent.1.7.0 \
-    octavius.1.2.0 \
-    result.1.3 \
+    octavius.1.2.1 \
+    result.1.4 \
     seq.base
--- rebase-forked-repos.sh
+++ rebase-forked-repos.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-for repo in core; do
+for repo in core re2; do
     cd ${repo}; 
     git pull --rebase https://github.com/janestreet/${repo}.git;
     cd ${OLDPWD};
