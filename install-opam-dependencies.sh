d783ab927204eede76de336ec61ae0d92506b7f5
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-04-07 20:06:53 +0100

    move some more libraries from opam to submodules

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -8,10 +8,8 @@ opam install -y \
     dune.2.4.0 \
     dune-configurator.2.4.0 \
     merlin.3.3.3 \
-    num.1.3 \
     ocamlbuild.0.14.0 \
     ocp-indent.1.8.1 \
-    octavius.1.2.2 \
     result.1.5 \
     seq.base
 
