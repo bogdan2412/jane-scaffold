04fddbe964a068dacf9e50a8cb696a403af0a525
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-04-16 19:48:24 +0100

    update opam packages

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -5,9 +5,9 @@ opam install -y \
     cmdliner.1.0.4 \
     ctypes.0.17.1 \
     ctypes-foreign.0.4.0 \
-    dune.2.4.0 \
-    dune-configurator.2.4.0 \
-    merlin.3.3.3 \
+    dune.2.5.0 \
+    dune-configurator.2.5.0 \
+    merlin.3.3.4 \
     ocamlbuild.0.14.0 \
     ocp-indent.1.8.1 \
     result.1.5 \
