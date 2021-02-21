2f15d7331913a7cb26c19a5b636966f26d3fa674
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2021-02-21 11:40:00 +0000

    update opam dependencies

--- install-opam-dependencies.sh
+++ install-opam-dependencies.sh
@@ -4,12 +4,12 @@ opam install -y \
     bos.0.2.0 \
     cmdliner.1.0.4 \
     ctypes.0.17.1 \
-    ctypes-foreign.0.4.0 \
-    dune.2.7.1 \
-    dune-configurator.2.7.1 \
+    ctypes-foreign.0.18.0 \
+    dune.2.8.2 \
+    dune-configurator.2.8.2 \
     merlin.3.4.2 \
     ocamlbuild.0.14.0 \
-    ocamlformat.0.16.0 \
+    ocamlformat.0.17.0 \
     ocp-indent.1.8.1 \
     result.1.5 \
     seq.base
